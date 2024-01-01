438158ccf265742727213c9295505a64f8b62ec8
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-01-01 18:15:07 -0500

    Estructura version 2

---
+++ Versión 2/Codigo/src/main/java/modelo/Asana.java
@@ -0,0 +1,135 @@
+package modelo;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Asana implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	final String SQL_SELECT_ALL = "SELECT * FROM asanas;";
+	private static ArrayList<Asana> listaAsanas = null;
+
+	
+	private String nombreEnIngles;
+    private String nombreEnEspañol;
+    private String nombreEnSans;
+    private String categoria;
+    private String rutaImgen;
+    
+    public Asana() {
+    }
+
+	public Asana(String nombreEnIngles, String nombreEnEspañol, String nombreEnSans) {
+    	super();
+        this.nombreEnIngles = nombreEnIngles;
+        this.nombreEnEspañol = nombreEnEspañol;
+        this.nombreEnSans = nombreEnSans;
+    }
+
+    public String getNombreEnIngles() {
+        return nombreEnIngles;
+    }
+
+    public void setNombreEnIngles(String nombreEnIngles) {
+        this.nombreEnIngles = nombreEnIngles;
+    }
+
+    public String getNombreEnEspañol() {
+        return nombreEnEspañol;
+    }
+
+    public void setNombreEnEspañol(String nombreEnEspañol) {
+        this.nombreEnEspañol = nombreEnEspañol;
+    }
+
+    public String getNombreEnSans() {
+        return nombreEnSans;
+    }
+
+    public void setNombreEnSans(String nombreEnSans) {
+        this.nombreEnSans = nombreEnSans;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getRutaImgen() {
+        return rutaImgen;
+    }
+
+    public void setRutaImgen(String rutaImgen) {
+        this.rutaImgen = rutaImgen;
+    }
+    
+    
+    public ArrayList<Asana> getAsanas(){
+    	listaAsanas = new ArrayList<Asana>();
+    	try {
+    		PreparedStatement pstm = BddConeccion.getConexion().prepareStatement(SQL_SELECT_ALL);
+			ResultSet rs = pstm.executeQuery();
+			
+			while(rs.next()) {
+				Asana asana = new Asana();
+				asana.setNombreEnIngles(rs.getString(2));
+				asana.setNombreEnEspañol(rs.getString(3));
+				asana.setNombreEnSans(rs.getString(4));
+				asana.setRutaImgen(rs.getString(5));
+				asana.setCategoria(rs.getString(6));
+				listaAsanas.add(asana);
+			}
+			BddConeccion.cerrar(rs);
+			BddConeccion.cerrar(pstm);
+			BddConeccion.cerrar();
+;		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listaAsanas;
+	}
+    
+    public Asana buscarPorNombre(String nombre,ArrayList<Asana> listaAsanas) {
+    	
+        for (Asana asana : listaAsanas) {        	
+            if (asana.getNombreEnSans().equalsIgnoreCase(nombre)) {
+                return asana;
+            }
+        }
+        return null; 
+    }
+    
+    public ArrayList<Asana> getAsanasPorCategoria(String categoria) {
+        listaAsanas = new ArrayList<Asana>();
+        final String SQL_SELECT_BY_CATEGORY = "SELECT * FROM asanas WHERE categoria = ?;";
+
+        try {
+            PreparedStatement pstm = BddConeccion.getConexion().prepareStatement(SQL_SELECT_BY_CATEGORY);
+            pstm.setString(1, categoria);
+            ResultSet rs = pstm.executeQuery();
+
+            while (rs.next()) {
+                Asana asana = new Asana();
+                asana.setNombreEnIngles(rs.getString(2));
+                asana.setNombreEnEspañol(rs.getString(3));
+                asana.setNombreEnSans(rs.getString(4));
+                asana.setRutaImgen(rs.getString(5));
+                asana.setCategoria(rs.getString(6));
+                listaAsanas.add(asana);
+            }
+
+            BddConeccion.cerrar(rs);
+            BddConeccion.cerrar(pstm);
+            BddConeccion.cerrar();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return listaAsanas;
+    }
+}
---
+++ Versión 2/Codigo/src/main/java/modelo/Morfema.java
@@ -0,0 +1,83 @@
+package modelo;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Morfema implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	final String SQL_SELECT_ALL = "SELECT * FROM morfemas;";
+	private static ArrayList<Morfema> listaMorfemas = null;
+	
+	private String nombreMorfema;
+    private String traduccion;
+    
+    public Morfema() {
+    }
+
+    public Morfema(String nombreMorfema, String traduccion) {
+        this.nombreMorfema = nombreMorfema;
+        this.traduccion = traduccion;
+    }
+
+    public String getNombreMorfema() {
+        return nombreMorfema;
+    }
+
+    public void setNombreMorfema(String nombreMorfema) {
+        this.nombreMorfema = nombreMorfema;
+    }
+
+    public String getTraduccion() {
+        return traduccion;
+    }
+
+    public void setTraduccion(String traduccion) {
+        this.traduccion = traduccion;
+    }
+    
+    public ArrayList<Morfema> getMorfemas(){
+    	listaMorfemas = new ArrayList<Morfema>();
+    	try {
+    		PreparedStatement pstm = BddConeccion.getConexion().prepareStatement(SQL_SELECT_ALL);
+			ResultSet rs = pstm.executeQuery();
+			
+			while(rs.next()) {
+				Morfema morfema = new Morfema();
+				morfema.setNombreMorfema(rs.getString(2));
+				morfema.setTraduccion(rs.getString(3));
+				listaMorfemas.add(morfema);
+			}
+			BddConeccion.cerrar(rs);
+			BddConeccion.cerrar(pstm);
+			BddConeccion.cerrar();
+;		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listaMorfemas;
+	}
+    
+    public Morfema buscarPorNombre(String nombre, ArrayList<Morfema> listaMorfemas ) {
+        for (Morfema morfema : listaMorfemas) {
+            if (morfema.getNombreMorfema().equalsIgnoreCase(nombre)) {
+                return morfema;
+            }
+        }
+        return null; 
+    }
+    public ArrayList<Morfema> buscarMorfemasEnPalabra(String palabra, ArrayList<Morfema> listaMorfemas) {
+        ArrayList<Morfema> morfemasEncontrados = new ArrayList<>();
+
+        for (Morfema morfema : listaMorfemas) {
+            if (palabra.toLowerCase().contains(morfema.getNombreMorfema().toLowerCase())) {
+                morfemasEncontrados.add(morfema);
+            }
+        }
+
+        return morfemasEncontrados;
+    }
+    
+}
